1dce2186e37156887e0d3ed54afe61f2cb547abb
nishadelias <nishad.elias@gmail.com>
2025-07-21 22:39:46 -0700

    Added documentation

--- CPU.h
+++ CPU.h
@@ -24,23 +24,23 @@ private:
 	int32_t registers[32];
 	ALU alu;
 
-	int32_t generate_immediate(uint32_t instruction, int opcode);
-    int32_t sign_extend(int32_t value, int bits);
-    bool check_address_alignment(uint32_t address, uint32_t bytes);
+	int32_t generate_immediate(uint32_t instruction, int opcode);	// Generates the immediate value for a given instruction based on its opcode
+    int32_t sign_extend(int32_t value, int bits);					// Performs sign extension on a given value for a specified bit width
+    bool check_address_alignment(uint32_t address, uint32_t bytes);	// Checks whether a memory access address is properly aligned
 
 public:
 	CPU();
 	unsigned long readPC();
-	void incPC();
-	string get_instruction(char *IM);
-	int get_register_value(int reg);
+	void incPC();							// Increments the program counter (PC) by 8 (i.e., one instruction)
+	string get_instruction(char *IM);		// Fetches a 32-bit instruction (8 hex characters) from instruction memory
+	int get_register_value(int reg);		// Returns the value stored in the specified register
 	bool decode_instruction(string inst, bool *regWrite, bool *aluSrc, bool *branch, bool *memRe, bool *memWr, bool *memToReg, bool *upperIm, int *aluOp,
 		unsigned int *opcode, unsigned int *rd, unsigned int *funct3, unsigned int *rs1, unsigned int *rs2, unsigned int *funct7);
 
-	void execute(int rd, int rs1, int rs2, int aluOp, int opcode, string inst);
+	void execute(int rd, int rs1, int rs2, int aluOp, int opcode, string inst);		// Executes a decoded instruction by performing ALU/memory operations and updating state.
 
-	int32_t read_memory(uint32_t address, bool is_byte);
-    void write_memory(uint32_t address, int32_t value, bool is_byte);
+	int32_t read_memory(uint32_t address, bool is_byte);				// Reads a byte or word from memory at the specified address
+    void write_memory(uint32_t address, int32_t value, bool is_byte);	// Writes a byte or word to memory at the specified address.
 	
 };
 
